888f71ca7307ffe610a59f7582d16076361518ad
RomanDMWB <rm1931100@gmail.com>
2022-12-04 22:29:07 +0300

    add vue

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-require("./app/routes")(app);
+require('./app/routes')(app);
 app.listen(port, () => {
-  console.log("We are live on " + port);
+    console.log('We are live on ' + port);
 });
