91c0aec8e40b9920253d0fe8a97d5c1db234ad09
Roma Makarov(RomanDMWB) <rm1931100@gmail.com>
2022-12-09 19:48:07 +0300

    update auth and login windows

--- script/app.js
+++ script/app.js
@@ -2,11 +2,19 @@ const app = new Vue({
     el:"#app",
     data:{
         page:"main",
-        modal:""
+        modal:"",
     },
     methods:{
         goAuth(){
             this.modal = "auth";
+        },
+        goLogin(){
+            this.modal = "login";
+        },
+        exitModal(e){
+            if(e.srcElement.classList.contains('modal-window'))
+            this.modal = "";
         }
     }
 })
+
